3ac5f0135355276c6c0b38b6a6b11b021a3909e8
Matvey Zhigalka <mzhigalka@gmail.com>
2023-09-18 19:54:37 +0300

    added scroll for header

--- js/main.js
+++ js/main.js
@@ -25,3 +25,63 @@ function open(event){
   })
 
 }
+
+function scrollNav(){
+  const navLinks = document.querySelectorAll('.menu__list-link')
+
+  navLinks.forEach(function(navLink){
+    navLink.addEventListener('click', function(event){
+      event.preventDefault()
+
+      const activeElem = document.querySelectorAll('.active')
+      
+      activeElem.forEach(function(activeElem){
+        activeElem.classList.remove('active')
+      })
+
+      this.classList.add('active')
+
+      const targetId = this.getAttribute('href').substring(1)
+      const targerElem = document.getElementById(targetId)
+      if(targerElem){
+        const offsetTop = targerElem.offsetTop - 160
+        window.scrollTo({
+          top: offsetTop,
+          behavior: 'smooth'
+        })
+      }
+    })
+  })
+}
+
+scrollNav()
+
+// let info = document.querySelector('.menu__list-link') 
+// let scrollAmount = 1600
+// let scrollDuration = 1000
+
+// info.addEventListener('click', function(event){
+//   event.preventDefault()
+
+//   var scrollTop = window.scrollY || document.documentElement.scrollTop;
+//   var targetScrollTop = scrollTop + scrollAmount;
+
+//   scrollToSmoothly(targetScrollTop, scrollDuration);
+// })
+
+// function scrollToSmoothly(targetPosition, duration) {
+//   var startPosition = window.scrollY || document.documentElement.scrollTop;
+//   var startTime = performance.now();
+  
+//   function scrollStep(timestamp) {
+//     var currentTime = timestamp - startTime;
+//     var progress = Math.min(currentTime / duration, 1);
+//     window.scrollTo(0, startPosition + progress * (targetPosition - startPosition));
+    
+//     if (progress < 1) {
+//       window.requestAnimationFrame(scrollStep);
+//     }
+//   }
+  
+//   window.requestAnimationFrame(scrollStep);
+// }
